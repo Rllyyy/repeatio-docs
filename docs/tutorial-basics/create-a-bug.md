3154b74ca90a91a841c0665c90d9e37b75151bbc
Niklas Fischer <nikii17.06@gmail.com>
2023-09-04 18:08:51 +0200

    initial commit

---
+++ docs/tutorial-basics/create-a-bug.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 7
+sidebar_label: "Test"
+---
+
+# Create a Bug
+
---
+++ src/pages/test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Test(): JSX.Element {
+  return <p>test</p>;
+
+  /*     {/* <Layout title={`Hello from ${siteConfig.title}`} description='Description will go into a meta tag in <head />'>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout> */
+}
