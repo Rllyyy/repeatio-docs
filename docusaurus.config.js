1d351d7c0ee998a32ed16341f8dec9067b627ea8
Niklas Fischer <nikii17.06@gmail.com>
2024-04-24 17:05:32 +0200

    add github workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,16 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://rllyyy.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/repeatio-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Rllyyy",
+  organizationName: "rllyyy",
   projectName: "repeatio-docs",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
