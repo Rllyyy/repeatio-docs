bc0a324f61d19cdbe421a6dddc1967dc5647fdeb
Niklas Fischer <nikii17.06@gmail.com>
2024-09-27 14:25:07 +0200

    revert changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: "https://rllyyy.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/repeatio-docs/branches/docusaurus-3/", // do this with env vars
+  baseUrl: "/repeatio-docs/", // do this with env vars
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
